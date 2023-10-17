8b174cc1096e3bbaaf918f00b4f5ab309fcc4896
Aidan Yong <courselearner01@gmail.com>
2023-10-17 14:09:44 +0800

    fixed

--- context/StateContext.js
+++ context/StateContext.js
@@ -60,19 +60,26 @@ export const StateContext = ({ children }) => {
     }
 
     const toggleCartItemQuantity = (id, value) => {
-        foundProduct = cartItems.find((item) => item._id === id)
-        index = cartItems.findIndex((product) => product._id === id);
-        const newCartItems = cartItems.filter((item) => item._id !== id)
+        const foundProduct = cartItems.find((item) => item._id === id);
+        const index = cartItems.findIndex((product) => product._id === id);
+        
+        if (value === 'inc') {
+            const updatedItem = { ...foundProduct, quantity: foundProduct.quantity + 1 };
+            const updatedCartItems = [...cartItems];
+            updatedCartItems[index] = updatedItem;
 
-        if(value === 'inc') {
-            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
-            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
-            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
-        } else if(value === 'dec') {
+            setCartItems(updatedCartItems);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.DefaultPrice);
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+        } else if (value === 'dec') {
             if (foundProduct.quantity > 1) {
-                setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
-                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
-                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+                const updatedItem = { ...foundProduct, quantity: foundProduct.quantity - 1 };
+                const updatedCartItems = [...cartItems];
+                updatedCartItems[index] = updatedItem;
+
+                setCartItems(updatedCartItems);
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.DefaultPrice);
+                setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
             }
         }
     }
